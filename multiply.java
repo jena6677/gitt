0fb78a9a93e479b2403af1b3333ce89c6e23db4b
Asutosh Jena <jenaasutosh2000@gmail.com>
2021-07-25 00:37:22 +0530

    adding multiply feature to asutosh/multiply branch

---
+++ multiply.java
@@ -0,0 +1,10 @@
+public class multiply {
+    public static void main(String[] args) 
+    {
+        int a = 8;
+        int b = 9;
+        int mult = a*b;
+        System.out.println(mult);    
+    }
+}
+
